264ca4ee0306b86e5ef923ca8d8d04c1ca468a7d
root <alazarworkneh@gmail.com>
2024-04-04 10:39:44 -0700

    m

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
-"""A module for web application deployment with Fabric."""
-import os
+"""Fabric script that generates a .tgz archive from the contents of the
+web_static folder of your AirBnB Clone repo, using the function do_pack"""
 from datetime import datetime
-from fabric.api import local, runs_once
+from fabric.api import local
 
 
-@runs_once
 def do_pack():
-    """Archives the static files."""
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    cur_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
-    )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        archize_size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
-    except Exception:
-        output = None
-    return output
-
+    """Creates archive from web_static directory"""
+    local("mkdir -p versions")
+    file = 'versions/web_static_{}.tgz'\
+        .format(datetime.strftime(datetime.now(), "%Y%m%d%I%M%S"))
+    comp = 'tar -cvzf {} web_static'.format(file)
+    tar_file = local(comp)
+    if tar_file.failed:
+        return None
+    return file
